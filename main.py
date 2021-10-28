ccde50fb476788ea789696f9b81073a389137cdc
Jordan <jordan@chude.co>
2021-10-27 20:47:48 -0400

    working html parser

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from bs4 import BeautifulSoup
 # READING FILE
 year_end_music = 'https://en.wikipedia.org/wiki/List_of_Billboard_Year-End_number-one_singles_and_albums'
 year_end_html = urlopen(year_end_music)
+soup = BeautifulSoup(year_end_html, 'html.parser')
 
-print(year_end_html)
+# TEST TEXT
+links = soup.find(title="Hey! Ba-Ba-Re-Bop")
+print (links)
+
+# print(year_end_html)
 # with open()
